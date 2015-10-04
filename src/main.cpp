97445115d46ec06218146c18e95fa530e0dcfe52
Gavrilov Seva <gavrilovseva@gmail.com>
2015-10-04 17:54:36 -0300

    Separado el código que genera vector de lineas a partir de un gz a una función.

--- src/main.cpp
+++ src/main.cpp
@@ -5,42 +5,66 @@
 #include <boost/iostreams/filtering_streambuf.hpp>
 #include <boost/iostreams/copy.hpp>
 #include <boost/iostreams/filter/gzip.hpp>
-#include <boost/tokenizer.hpp>
 
 #include "vendor/csv.h"
+
+#include <time.h>
+
+// using namespace arma;
+using namespace std;
+using namespace boost;
+
+
+vector<vector<string>> getLines(std::string filename) {
+
+  ifstream file(filename, ios_base::binary);
+  iostreams::filtering_streambuf<iostreams::input> inbuf;
+  inbuf.push(iostreams::gzip_decompressor());
+  inbuf.push(file);
+
+  istream stream(&inbuf);
+
+  vector<vector<string>> vec;
+  string headers;
+  getline(stream, headers);
+
+  cout << "Headers: " << headers << endl;
+
+  vector<string> line;
+  string token;
+
+  int ch;
+  while (EOF != (ch = stream.get())) {
+    switch(ch) {
+      case '\n':
+          vec.push_back(line);
+          line.clear();
+          token.clear();
+          break;
+      case ',':
+          line.push_back(token);
+          token.clear();
+          break;
+      default:
+          token.push_back(ch);
+    }
+  }
+
+  return vec;
+}
 
 
 int main(int argc, char const *argv[])
 {
-  using namespace std;
-  using namespace boost;
+  clock_t tStart = clock();
 
-  ifstream file("../data/trainmin.csv.gz", ios_base::binary);
-  iostreams::filtering_streambuf<iostreams::input> inbuf;
-  inbuf.push(iostreams::gzip_decompressor());
-  inbuf.push(file);
-  // iostreams::copy(inbuf, cout);
+  vector<vector<string>> vec = getLines("../data/train.csv.gz");
 
-  std::istream instream(&inbuf);
-
-  vector<vector<string>> vec;
-
-  std::string headers;
-  std::getline(instream, headers);
-
-  cout << "Headers: " << headers << endl;
-
-  std::string line;
-  while(std::getline(instream, line)) {
-    vector<string> lineVec;
-    tokenizer<escaped_list_separator<char>> tok(line);
-    lineVec.assign(tok.begin(),tok.end());
-    vec.push_back(lineVec);
-  }
+  printf("Getline: %.2fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC);
 
   for(auto &item : vec) {
-    copy(item.begin(), item.end(), ostream_iterator<string>(cout, "|"));
-    cout << endl;
+    // copy(item.begin(), item.end(), ostream_iterator<string>(cout, "|"));
+    // cout << endl;
   }
 
   return 0;
