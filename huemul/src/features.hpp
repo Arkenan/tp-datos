87ad8b277cb5f513d4225a407822807ff4c3cbf2
Gavrilov Seva <gavrilovseva@gmail.com>
2015-11-10 00:07:00 -0300

    Agregada una breve descripción de las funciones en io.hpp y features.hpp.

--- huemul/src/features.hpp
+++ huemul/src/features.hpp
@@ -10,10 +10,34 @@
 
 using namespace arma;
 
+/**
+ * A partir del vector parseado del set de entrenamiento, genera un diccionario
+ * de clases, donde a cada string de clase le asigna un numero unico.
+ * La columna de clases por defecto es la segunda.
+ * Por ejemplo:
+ *
+ * ID | Clase         {
+ * 0  | Rojo            Azul: 0
+ * 1  | Azul    =>      Rojo: 1
+ * 2  | Rojo          }
+ */
 map<string, int> getLabelMap(parsedStrings vec);
+
+/**
+ * A partir del set de entrenamiento y el diccionario de clases, genera el
+ * un vector de clases correspondientes a cada sample.
+ */
 mat getLabels(parsedStrings vec, map<string, int> labelsMap);
+
+/**
+ * Convierte el set de entrenamiento en forma de strings en una matriz de
+ * numeros.
+ */
 mat getFeatures(parsedStrings vec, int datesCol);
 
+/**
+ * Normaliza las columnas de la matrix X
+ */
 mat scaleFeatures(mat X, mat mu, mat sigma);
 
 #endif
--- huemul/src/io.hpp
+++ huemul/src/io.hpp
@@ -18,8 +18,22 @@ typedef vector<vector<string>> parsedStrings;
 using namespace arma;
 using namespace boost::iostreams;
 
+/**
+ * Descomprime y parsea un archivo separado por comas comprimido con gzip.
+ * Retorna un vector de lineas, que a su vez son vectores de strings, donde
+ * cada linea es una fila del archivo, y cada string es un elemento.
+ * Descarta la primera linea.
+ */
 parsedStrings getLines(std::string filename);
+
+/**
+ * Igual que getLines, solo que toma las primeras @numlines filas del archivo.
+ */
 parsedStrings getLines(std::string filename, int numlines);
+
+/**
+ * Escribe una matriz de armadillo a disco en formato csv, comprimido con gz.
+ */
 void writeMatrix(mat matrix, string filename);
 
 #endif
