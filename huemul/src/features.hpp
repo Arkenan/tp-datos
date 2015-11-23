5d87e855fa342003130954c04a412f4e1b142910
Gavrilov Seva <gavrilovseva@gmail.com>
2015-11-23 19:02:05 -0300

    Agregada documentación al módulo de features.

--- huemul/src/features.hpp
+++ huemul/src/features.hpp
@@ -41,14 +41,32 @@ mat scaleFeatures(mat X, mat mu, mat sigma);
  */
 mat scaleFeatures(mat X, mat mu, mat sigma, int colums);
 
+
+/**
+ * Mapea valores unicos del set de entrenamiento a una clase unívoca
+ */
 class LabelBinarizer {
     public:
       LabelBinarizer();
 
+      /**
+       * A partir de una columna del set, asigna a cada valor único una clase.
+       * La clase es un número de 0 a N - 1, siendo N la cantidad de valores
+       * únicos.
+       */
       void fit(parsedStrings vec, int colnum);
+
+      /**
+       * Devuelve una matriz binária con cada fila correspondiente a la fila
+       * del set, con un 1 en la columna correspondiente a la clase, y cero en
+       * el resto.
+       */
       mat transform(parsedStrings vec, int colnum);
+
+      /**
+       * Idem anterior, pero se le puede especificar la cantidad de columnas.
+       */
       mat transform(parsedStrings vec, int column, int features);
-      sp_mat transformSparse(parsedStrings vec, int column);
 
       map<string, int> getLabels();
     private:
@@ -57,6 +75,14 @@ class LabelBinarizer {
 };
 
 
+/**
+ * Un binarizador para las direcciones. Hace un preprocesamiento al campo de
+ * dirección sacando el/los nombres de calles unívocos.
+ *
+ * Por ejemplo:
+ * OAK ST / LAGUNA ST -> [LAGUNA, OAK]
+ * 600 Block of 47TH AV -> [47TH]
+ */
 class StreetBinarizer:
   public LabelBinarizer {
   public:
@@ -67,6 +93,26 @@ class StreetBinarizer:
     map<string, int> labelsMap_;
 };
 
+
+/**
+ * Genera las matrices de features para los sets de test y sets de
+ * entrenamiento.
+ *
+ * Las features existentes son (por columna):
+ * 0 -> Año
+ * 1 -> Mes
+ * 2 -> Día
+ * 3 -> Hora
+ * 4 -> X
+ * 5 -> Y
+ * 6 .. 9 -> Estación
+ * 10 -> Si es fin de semana
+ * 11 -> Si es de día
+ * 12 -> Si es una intersección de calles
+ * 13 .. 24 -> Distritos
+ * 25 .. 31 -> Día de semana
+ * 27 .. 159 -> Calles (Opcional)
+ */
 class FeatureConverter {
     public:
       FeatureConverter(parsedStrings train, parsedStrings test);
